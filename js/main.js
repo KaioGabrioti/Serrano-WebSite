902a35ae6bd3433bf9716bb77d4508003358eb4b
Kaio Gabrioti <kgabrioti@gmail.com>
2023-08-08 20:16:30 -0300

    ajustando pagina o clube

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
 	Version: 1.0
  ------------------------------------
  ====================================*/
-
-
-
 'use strict';
 
 $(window).on('load', function() {
@@ -44,6 +41,5 @@ $(window).on('load', function() {
 	$(".counter").countdown("2023/08/29", function(event) {
 		$(this).html(event.strftime("<div class='counter-item'><h4>%D</h4>Days</div>" + "<div class='counter-item'><h4>%H</h4>hours</div>" + "<div class='counter-item'><h4>%M</h4>Mins</div>" + "<div class='counter-item'><h4>%S</h4>secs</div>"));
 	});
-
 
 })(jQuery);
